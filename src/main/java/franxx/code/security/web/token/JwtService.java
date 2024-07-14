640f42fbf06202b3a27c0ab4f75db1d404c57d3f
Hilmi AM <imiia75775@proton.me>
2024-07-14 11:51:40 +0700

    create jwt with a user with secret key

--- src/main/java/franxx/code/security/web/token/JwtService.java
+++ src/main/java/franxx/code/security/web/token/JwtService.java
@@ -1,7 +1,38 @@
 package franxx.code.security.web.token;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.time.Instant;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class JwtService {
+  private static final String SECRET_KEY = "7B2FDA14370B8251D762A22C881FD6F21446D9FC9AF8811755AEBF318081F081F743BDCD89BE5CFCBDB68F822589AB2B6C216638F50060844C7D9CE367FDB315";
+  private static final Long VALID_TOKEN = TimeUnit.MINUTES.toMillis(30);
+
+  public String generateToken(UserDetails userDetails) {
+    Map<String, String> claims = new HashMap<>();
+    claims.put("pacman", "arch-linux");
+
+    return Jwts.builder()
+        .subject(userDetails.getUsername())
+        .claims(claims)
+        .issuedAt(Date.from(Instant.now()))
+        .expiration(Date.from(Instant.now().plusMillis(VALID_TOKEN)))
+        .signWith(generateSecretKey())
+        .compact();
+  }
+
+  private SecretKey generateSecretKey() {
+    byte[] decode = Base64.getDecoder().decode(SECRET_KEY);
+    return Keys.hmacShaKeyFor(decode);
+  }
 }
